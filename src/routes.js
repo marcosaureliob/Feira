4c68df809a27e15cb48ff3b6f00596c08be46101
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-05-09 09:46:50 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import { UsuarioProvider } from "common/contexts/Usuario";
+import Carrinho from "pages/Carrinho";
+import Feira from "pages/Feira";
+import Login from "pages/Login";
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <UsuarioProvider>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/feira">
+            <Feira />
+          </Route>
+        </UsuarioProvider>
+        <Route path="/carrinho">
+          <Carrinho />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
